97031b37e29ac6a0625a67b5c0849342c5fb2b8c
A1suluu <baisheva2004@gmail.com>
2022-01-10 14:21:02 +0600

    create new file and finished hw.036

---
+++ hw.036/hw2/app.js
@@ -0,0 +1,19 @@
+let i = 1, j = 1;
+let userHeight = prompt("Введите число");
+let freeSpace = "", stars = "", leftStars = "";
+
+
+while (i < userHeight) {
+  freeSpace = "";
+  stars = "";
+  for (j = 0; j < userHeight - i; j++) {
+      freeSpace += "*"
+    }; 
+  for (j = 1; j < 2 * i + 1; j++) { 
+      stars += "/"
+    };
+
+  console.log(freeSpace + stars);
+  i++;
+}
+
