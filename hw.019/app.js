8a43a7f12e6cb76ee135a3c5ab1d7280ac4da4ee
A1suluu <baisheva2004@gmail.com>
2022-03-25 09:49:00 +0600

    hw.019

---
+++ hw.019/app.js
@@ -0,0 +1,8 @@
+let widthofroom = prompt ("Ширина комнаты")
+let length = prompt ("Длина комнаты")
+let height = prompt ("Высота комнаты")
+
+let s = 2*(Number(widthofroom) + Number(length)) * Number(height)
+
+console.log(("Общая площадь всех стен: ") + (s) + ("м2"))
+
